b04f0f7972bca9a1c522752440d7c01096c1ea46
Mateus <mateuscorreiaazevedo@gmail.com>
2023-05-18 02:33:30 -0300

    chore: adjusting app.listen for mobile app

--- src/server.ts
+++ src/server.ts
@@ -17,4 +17,4 @@ app.register(jwt, {
 app.register(memoriesRoutes)
 app.register(authRoutes)
 
-app.listen({ port: 3333 }).then(() => console.log('HTTP server running on http://localhost:3333'))
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => console.log('HTTP server running on http://localhost:3333'))
