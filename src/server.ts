ab9b91058ce432bdbdb4dd9ed89531e78be54b81
Mateus <mateuscorreiaazevedo@gmail.com>
2023-05-19 17:03:30 -0300

    refactor: adjusting app.listen

--- src/server.ts
+++ src/server.ts
@@ -29,4 +29,11 @@ app.register(memoriesRoutes)
 app.register(authRoutes)
 app.register(uploadRoutes)
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => console.log('HTTP server running on http://localhost:3333'))
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0'
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
